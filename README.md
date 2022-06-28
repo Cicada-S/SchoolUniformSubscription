3be1570f03292f90eac56d4a2b3087d8692c50c3
Cicada-S <202586563@qq.com>
2022-06-28 14:00:44 +0800

    🎉 init project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SchoolUniformSubscription
+
+## 安装依赖
+
+```
+  npm install
+```
+
+then 工具 -> 构建npm
---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+App({
+  onLaunch() {
+    wx.cloud.init({
+      env: 'school-shop-2gg4adfl2d11e6fb',
+      traceUser: true
+    })
+  }
+})
