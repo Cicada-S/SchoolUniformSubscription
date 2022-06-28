ad10d8cc481dbe1a202dead682e24239b4d695f9
Cicada-S <202586563@qq.com>
2022-06-28 18:08:58 +0800

    Home

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,47 @@
 Page({
+  data: {
+    chart: '/static/images/home/chart.png',
+    picUrl: '/static/images/home/clothes.png',
+    dataList: [
+      {
+        id: 1,
+        title: '冬季园服新款（演示）qweq',
+        picUrl: '/static/images/home/clothes1.png',
+      },
+      {
+        id: 2,
+        title: '冬季园服新款（演示）',
+        picUrl: '/static/images/home/clothes1.png',
+      },
+      {
+        id: 3,
+        title: '冬季园服新款（演示）',
+        picUrl: '/static/images/home/clothes1.png',
+      },
+      {
+        id: 4,
+        title: '冬季园服新款（演示）qweq',
+        picUrl: '/static/images/home/clothes1.png',
+      }
+    ]
+  },
 
+  // 页面初始化 options为页面跳转所带来的参数
+  onLoad: function (options) {
+
+  },
+
+  onJump: e => {
+    console.log(e.target.id);
+
+    let type = {
+      product,
+      code,
+      school
+    }
+    
+    wx.navigateTo({
+      
+    })
+  }
 })
