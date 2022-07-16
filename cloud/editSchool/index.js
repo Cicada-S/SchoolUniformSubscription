2ebbbca1587c9c1000134fdc12f92ef8b15216bb
Cicada <202586563@qq.com>
2022-07-16 16:00:07 +0800

    editSchool cloudFunction

--- cloud/editSchool/index.js
+++ cloud/editSchool/index.js
@@ -5,8 +5,14 @@ const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let {id, name, address, logo} = event
+  let {id, name, address, logo, grade} = event
   try {
+    // 删除原有的年级
+    await db.collection('Grade').where({
+      schoolId: id
+    }).remove()
+
+    // 修改学校信息
     await db.collection('School').doc(id).update({
       data: {
         name,
@@ -15,6 +21,17 @@ exports.main = async (event, context) => {
       }
     })
 
+    // 添加年级
+    grade.forEach(async (item, index) => {
+      let newItem = {...item}
+      newItem.order = index
+      newItem.schoolId = id
+      await db.collection('Grade').add({
+        data: newItem
+      })
+    });
+
+    // 成功返回
     return {
       code: 0,
       success: true 
@@ -22,7 +39,7 @@ exports.main = async (event, context) => {
   }
   catch (err) {
     console.error('transaction error')
-
+    // 失败返回
     return {
       code: 1,
       success: false
