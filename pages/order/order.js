05db55872db3c2222dea3932167dd84c2a79fe84
Cicada <202586563@qq.com>
2022-08-02 18:03:46 +0800

    order

---
+++ pages/order/order.js
@@ -0,0 +1,12 @@
+// pages/order/order.js
+Page({
+  data: {
+    studentInfo: {
+      id: 1,
+      name: 'Cicada',
+      Grade: '一年级 1班',
+      school: '清华大学幼儿园',
+      path: '/static/images/schoolShop/avatar.png',
+    },
+  }
+})
