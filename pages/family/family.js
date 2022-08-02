af699d137929a7d426db4bb79fd8992f1c77a446
Cicada <202586563@qq.com>
2022-08-02 11:36:28 +0800

    family

--- pages/family/family.js
+++ pages/family/family.js
@@ -1,4 +1,56 @@
 // pages/family/family.js
+const app = getApp()
+
+const db = wx.cloud.database()
+const student = db.collection('Student')
+
 Page({
-  data: {}
+  data: {
+    bottomLift: app.globalData.bottomLift, 
+    family: [
+      {
+        id: 1,
+        name: 'Cicada',
+        path: '/static/images/family/1.jpg',
+        schoolInfo: {
+          school: '清华大学幼儿园',
+          grade: '一年级',
+          class: '1班'
+        }
+      },
+      {
+        id: 2,
+        name: 'Ting',
+        path: '/static/images/family/2.jpg',
+        schoolInfo: {
+          school: '清华大学幼儿园',
+          grade: '一年级',
+          class: '1班'
+        }
+      }
+    ]
+  },
+
+  // 页面初始化
+  onLoad(options) {
+    console.log('页面初始化', options)
+    let id = '16db756f62ce61f80ac247284ecff688'
+
+    // this.getStudent(id)
+  },
+
+  getStudent(id) {
+    student.where({  }).get()
+    then(res => {
+      console.log(res)
+    })
+  },
+
+  // 编辑
+  toEditStudentInfo(event) {
+    let id = event.currentTarget.id
+    wx.navigateTo({
+      url: `/pages/editStudentInfo/editStudentInfo?id=${id}`
+    })
+  }
 })
