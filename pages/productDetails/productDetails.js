8cbb00b206f1d05a65b03c3898fed21072d1138a
Cicada <202586563@qq.com>
2022-08-08 16:43:18 +0800

    Swiper component

--- pages/productDetails/productDetails.js
+++ pages/productDetails/productDetails.js
@@ -1,10 +1,6 @@
 // pages/productDetails/productDetails.js
 Page({
   data: {
-    indicatorColor: 'rgba(64,127,245, .3)',
-    indicatorActiveColor: '#407FF5',
-    interval: 5000,
-    duration: 500,
     imageList: [
       '/static/images/productDetails/img (1).png',
       '/static/images/productDetails/img (2).png',
