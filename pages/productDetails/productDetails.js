e71992f70bb57e39914e51e9a287fe07389a5b61
Cicada <202586563@qq.com>
2022-08-08 18:00:02 +0800

    productDetails

--- pages/productDetails/productDetails.js
+++ pages/productDetails/productDetails.js
@@ -6,5 +6,27 @@ Page({
       '/static/images/productDetails/img (2).png',
       '/static/images/productDetails/img (3).png',
     ],
+    detailsImage: [
+      {
+        _id: "1",
+        path: '/static/images/productDetails/img (1).png',
+      },
+      {
+        _id: "2",
+        path: '/static/images/productDetails/img (2).png',
+      },
+      {
+        _id: "3",
+        path: '/static/images/productDetails/img (3).png',
+      },
+      {
+        _id: "4",
+        path: '/static/images/productDetails/img (4).png',
+      },
+      {
+        _id: "5",
+        path: '/static/images/productDetails/img (5).png',
+      }
+    ]
   }
 })
