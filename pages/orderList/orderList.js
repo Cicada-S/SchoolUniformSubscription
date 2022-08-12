25d798d59c52afac2eab5ca6714f51ac0f77895b
Cicada <202586563@qq.com>
2022-08-12 15:24:49 +0800

    getOrderList cloudFunction

--- pages/orderList/orderList.js
+++ pages/orderList/orderList.js
@@ -1,6 +1,23 @@
 // pages/orderList/orderList.js
 Page({
   data: {
-    
+    orderList: []
+  },
+
+  // 页面初始化
+  onLoad(options) {
+    console.log(options)
+
+    // this.getOrderList(options.id)
+  },
+
+  // 获取订单列表
+  getOrderList(id) {
+    wx.cloud.callFunction({
+      name: 'getOrderList',
+      data: { sellQrCodeId: id }
+    }).then(res => {
+      console.log(res)
+    })
   }
 })
