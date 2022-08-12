dfd4824e3a54bacaadfab080b580295e405e7031
Cicada <202586563@qq.com>
2022-08-12 16:19:21 +0800

    orderList and getOrderList cloudFunction

--- cloud/getOrderList/index.js
+++ cloud/getOrderList/index.js
@@ -8,4 +8,21 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   console.log(event)
+
+  try {
+    db.collection('Grade').where({schoolId: event.schoolId}).get()
+    .then(res => {
+      console.log(res)
+    })
+
+    db.collection('Order').where({sellQrCodeId: event.sellQrCodeId}).get()
+    .then(res => {
+      console.log(res)
+    })
+
+
+  }
+  catch(err) {
+    console.log(err)
+  }
 }
--- pages/orderList/orderList.js
+++ pages/orderList/orderList.js
@@ -1,21 +1,35 @@
 // pages/orderList/orderList.js
 Page({
   data: {
-    orderList: []
+    orderList: [],
+    option1: [
+      { text: '全校', value: 0 },
+      { text: '一年级', value: 1 },
+      { text: '二年级', value: 2 },
+    ],
+    option2: [
+      { text: '全年级', value: 'a' },
+      { text: '1班', value: 'b' },
+      { text: '2班', value: 'c' },
+    ],
+    value1: 0,
+    value2: 'a',
   },
 
   // 页面初始化
   onLoad(options) {
     console.log(options)
+    let sellQrCodeId = '0a4ec1f962f45b2c19049662135c755d'
+    let schoolId = '8f75309d62ea237d102837180c8273de'
 
-    // this.getOrderList(options.id)
+    this.getOrderList(sellQrCodeId, schoolId)
   },
 
   // 获取订单列表
-  getOrderList(id) {
+  getOrderList(sellQrCodeId, schoolId) {
     wx.cloud.callFunction({
       name: 'getOrderList',
-      data: { sellQrCodeId: id }
+      data: { sellQrCodeId, schoolId}
     }).then(res => {
       console.log(res)
     })
