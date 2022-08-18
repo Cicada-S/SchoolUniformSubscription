354a374a88eee0225a1d12c58c5026ffb8c695f7
Cicada <202586563@qq.com>
2022-08-18 17:08:03 +0800

    Purchase list (sub table)

--- cloud/exportOrderList/index.js
+++ cloud/exportOrderList/index.js
@@ -5,7 +5,7 @@ cloud.init({
 })
 const db = cloud.database()
 
-//操作excel用的类库
+// 操作excel用的类库
 const xlsx = require('node-xlsx')
 
 // 云函数入口函数
@@ -28,13 +28,24 @@ exports.main = async (event, context) => {
       return prev
     }, [])
 
+    console.log(newOrder)
+
     // 1, 定义excel表格名
-    let dataCVS = `${newOrder[0].sellQrCodeTitle}.xlsx`
+    let dataCVS = `${newOrder[0].sellQrCodeId}.xlsx`
 
     // 2, 定义存储数据的
-    let alldata = []
-    let row = ['id', '学校', '年级', '班级', '姓名', '性别', '下单时间', '商品名称', '购买数量', '规格', '备注' ] //表属性
-    alldata.push(row)
+    let summaryList = []
+    let summaryRow = ['性别', '规格', '产品名称', '总计']
+    summaryList.push(summaryRow)
+
+    /* for (let key in newOrder) {
+      let arr = []
+    } */
+
+
+    let detailedList = []
+    let detailedRow = ['id', '学校', '年级', '班级', '姓名', '性别', '下单时间', '商品名称', '购买数量', '规格', '备注' ] //表属性
+    detailedList.push(detailedRow)
 
     for (let key in newOrder) {
       let arr = []
@@ -49,10 +60,10 @@ exports.main = async (event, context) => {
       arr.push(newOrder[key].orderProduct[0].amount)
       arr.push(newOrder[key].orderProduct[0].specification)
       arr.push(newOrder[key].remark)
-      alldata.push(arr)
+      detailedList.push(arr)
     }
 
-    // 定制纸张规格
+    // 证订明细表 定制纸张规格
     const sheetOptions = {
       // 自定义列宽
       '!cols': [
@@ -62,16 +73,15 @@ exports.main = async (event, context) => {
       ]
     }
 
-    //3，把数据保存到excel里
-    var buffer = xlsx.build([{
-      name: "mySheetName",
-      data: alldata
-    }], {sheetOptions})
+    // 3, 把数据保存到excel里
+    var buffer = xlsx.build([
+      { name: "证订明细表", data: detailedList, options: sheetOptions }
+    ])
 
-    //4，把excel文件保存到云存储里
+    // 4, 把excel文件保存到云存储里
     let result =  await cloud.uploadFile({
       cloudPath: dataCVS,
-      fileContent: buffer, //excel二进制文件
+      fileContent: buffer, // excel二进制文件
     })
 
     // 成功返回
