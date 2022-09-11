701e1a26efaeac4e279957d16ed0c9ae025f0cf0
memoryguard <oncwnuOdi0axaFLtm-3GSSjja56M@git.weixin.qq.com>
2022-09-11 11:15:22 +0800

    correct total price

--- cloud/pay/index.js
+++ cloud/pay/index.js
@@ -33,7 +33,7 @@ exports.main = async (event, context) => {
     "outTradeNo": event.orderId, //不能重复，否则报错
     "spbillCreateIp": "127.0.0.1", //就是这个值，不要改
     "subMchId": "1628494787",  //你的商户号,
-    "totalFee": totalPrice,  //单位为分 *100
+    "totalFee": totalPrice * 100,  //单位为分 *100
     // "envId": "cloud1-9ghc7y1j0db0f20d",  //填入你的云开发环境ID
     "envId": "prod-5gbrg2v163ae3d24",  //填入你的云开发环境ID
     // "envId": cloud.DYNAMIC_CURRENT_ENV,  //填入你的云开发环境ID
